72e2630c8e7a022fdfdc70e9c9519bd3aebe30da
FabricioMessias <fabriciomessias@live.com>
2021-08-23 18:59:15 -0300

    Alteracoes nas noticias home

---
+++ js/slides.js
@@ -0,0 +1,16 @@
+var i = 0;
+transicaoSlide();
+
+function transicaoSlide() {
+    let j;
+    let slides = document.getElementsByClassName("slide");
+    for (j = 0; j < slides.length; j++) {
+        slides[j].style.display = "none";
+    }
+    i++;
+    if (i > slides.length) { 
+        i = 2 
+    }
+    slides[i - 1].style.display = "block";
+    setTimeout(transicaoSlide, 7000);
+}
